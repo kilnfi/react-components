f5cb2e08cea16a41ddde8650992bfc91b5462fbe
Arno Simon <arnos29@gmail.com>
2022-09-13 15:41:33 +0200

    v1.0.1

--- src/hooks/useEthStats.ts
+++ src/hooks/useEthStats.ts
@@ -3,7 +3,6 @@ import { fetcher } from "../api/api";
 
 export type EthereumStats = {
   networkApy: number,
-  supplyStakedPercent: number,
   isLoading: boolean,
   isError: boolean,
 };
@@ -12,17 +11,15 @@ const useEthStats = (apiUrl: string, apiKey: string): EthereumStats => {
   const {
     data: apiData,
     error: apiError,
-  } = useSWR(apiUrl !== '' ? '/v0/eth/network-stats' : null, (url) => fetcher(url, {
+  } = useSWR(apiUrl !== '' ? '/v1/eth/network-stats' : null, (url) => fetcher(url, {
     apiUrl,
     apiKey,
   }));
 
-  const supplyStakedPercent = apiData?.data?.supply_staked_percent ?? 0;
   const networkApy = apiData?.data?.apy ?? 0;
 
   return {
     networkApy,
-    supplyStakedPercent,
     isLoading: !apiError && !apiData,
     isError: Boolean(apiError),
   };
